2e7be3aa67d2da7e1f4b0863e603ea26dadc283a
nadvolod <nadvolod@gmail.com>
2020-08-29 13:54:42 -0400

    delete unecessary files

--- testing-js/assertion-library.js
+++
@@ -1,9 +0,0 @@
-const sum = (a, b) => a - b;
-//const subtract = (a, b) => a - b;
-
-const result = sum(10, 5);
-const expected = 15;
-
-if (result !== expected) {
-  throw new Error(`${result} is not equal to ${expected}`);
-}
