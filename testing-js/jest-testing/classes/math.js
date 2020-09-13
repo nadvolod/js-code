6b8cf35783f0695415627e1816f7bb0455105d52
nadvolod <nadvolod@gmail.com>
2020-09-13 09:26:11 -0400

    replace with multiply function

--- testing-js/jest-testing/classes/math.js
+++ testing-js/jest-testing/classes/math.js
@@ -1,9 +1,5 @@
-function factorialize(num) {
-    if (num < 0) return -1;
-    else if (num == 0) return 1;
-    else {
-        return num * factorialize(num - 1);
-    }
+function multiply(a, b) {
+    return a * b;
 }
 
-module.exports = { factorialize }
+module.exports = { multiply }
