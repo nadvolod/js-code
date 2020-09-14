4b2726cd1f913de128ac68943d85179aacc9e7ee
nadvolod <nadvolod@gmail.com>
2020-09-14 15:35:17 -0400

    mocking a chunk call

--- testing-js/jest-testing/__tests__/mocking.spec.js
+++ testing-js/jest-testing/__tests__/mocking.spec.js
@@ -1,6 +1,29 @@
+const lodash = require('lodash')
+const {Users} = require('../classes/')
+const axios = require('axios')
 
 //Mock a single call
 // https://lodash.com/docs/4.17.15#chunk
+test('should mock chunk call',()=>{
+    lodash.chunk = jest.fn(()=> 'fakeChunk')
 
+    const result = lodash.chunk(['a','b','c'], 2)
+    console.log(result)
+    expect(result).toBe('fakeChunk')
+    expect(lodash.chunk).toHaveBeenCalled()
+    expect(lodash.chunk).toHaveBeenCalledWith(['a', 'b', 'c'], 2)
+})
 
 //Mock an entire module
+jest.mock('axios');
+test('should fetch users', () => {
+    const users = [{name: 'Bob'}]
+    const fakeResponse = {data: users}
+    //axios.get() should return our fakeResponse
+    axios.get.mockResolvedValue(fakeResponse);
+    console.log(Users.all())
+    // or you could use the following depending on your use case:
+    // axios.get.mockImplementation(() => Promise.resolve(resp))
+
+    return Users.all().then(data => expect(data).toEqual(users))
+});
