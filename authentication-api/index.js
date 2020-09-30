0b1271efa991cba55aefe0624c5d50bfe4cae50a
nadvolod <nadvolod@gmail.com>
2020-09-30 13:29:35 -0400

    establish connection to real db

--- authentication-api/index.js
+++ authentication-api/index.js
@@ -1,5 +1,11 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+
+mongoose.connect(
+    'mongodb+srv://nikolay1:Test12345@cluster0.azx4b.mongodb.net/test?retryWrites=true&w=majority',
+    { useNewUrlParser: true },
+    ()=> console.log('Connected to db!'))
 
 const authRoute = require('./routes/auth');
 
