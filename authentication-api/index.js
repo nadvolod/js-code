46d5a488e1e0cbb23bdf791bbe05915a0b4e8ba8
nadvolod <nadvolod@gmail.com>
2020-10-01 16:20:33 -0400

    add working /register request for a user

--- authentication-api/index.js
+++ authentication-api/index.js
@@ -2,17 +2,21 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const dotenv = require('dotenv')
+//Import Routes
+const authRoute = require('./routes/auth');
 
 //needed to set our environment variables that are coming from .env file
 dotenv.config();
 
+//Connect to DB
 mongoose.connect(
     process.env.DB_CONNECT,
     { useNewUrlParser: true },
     ()=> console.log('Connected to db!'))
 
-const authRoute = require('./routes/auth');
-
+//Middleware
+app.use(express.json());
+//Route middleware
 app.use('/api/user', authRoute);
 
 app.listen(3001, ()=> console.log('Server is running'))
--- authentication-api/routes/auth.js
+++ authentication-api/routes/auth.js
@@ -1,15 +1,21 @@
 const router = require('express').Router();
+const User = require('../model/Users');
 
 //final url will be host/api/user/register
 //coming from app.use('/api/user', authRoute); in index.js
-router.post('/register', (req,res) => {
-    res.send('Register')
-})
+router.post('/register', async(req,res) => {
+    const user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+    });
 
-//final url will be host/api/user/login
-//coming from app.use('/api/user', authRoute); in index.js
-router.post('/login', (req,res) => {
-    res.send('Login')
-})
+    try{
+        const savedUser = await user.save();
+        res.send(savedUser);
+    }catch (err){
+        res.status(400).send(err);
+    }
+});
 
 module.exports = router;
