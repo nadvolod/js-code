bca0c5d237c7c3fbb77838291054546c2b9e8be2
nadvolod <nadvolod@gmail.com>
2020-11-28 19:06:10 -0500

    Add validation to make sure that we can't add a 2nd user

--- authentication-api/routes/auth.js
+++ authentication-api/routes/auth.js
@@ -6,8 +6,6 @@ const {Validation} = require('../validation');
 //coming from app.use('/api/user', authRoute); in index.js
 router.post('/register', async(req,res) => {
     //validate the data before we make a user
-    //this will return the entire validation object
-    //const validation = Joi.validate(req.body, schema);
 
     //this will return only the error part instead of the entire object
     const {error} = Validation.registerValidation(req.body);
@@ -15,6 +13,10 @@ router.post('/register', async(req,res) => {
     //res.send(error.details[0].message);
 
     if(error) return res.status(400).send(error.details[0].message);
+
+    //check if the user is already in the DB
+    const emailExists = await User.findOne({email: req.body.email});
+    if(emailExists) return res.status(400).send('Email already exists');
 
     const user = new User({
         name: req.body.name,
