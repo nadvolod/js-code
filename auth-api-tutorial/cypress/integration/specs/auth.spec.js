6096c00bde5e6bd4a8d9cccb39d42526a270f9a3
nadvolod <nadvolod@gmail.com>
2021-01-17 17:11:11 -0500

    Add code for auth

--- auth-api-tutorial/cypress/integration/specs/auth.spec.js
+++ auth-api-tutorial/cypress/integration/specs/auth.spec.js
@@ -1,25 +1,65 @@
-it('/register creates user', ()=> {
-    let body = {
-        name: 'TestName',
-        email: 'foo@bar.com',
-        password: 'Test0987'
-    }
-    //cypress assertions
-    cy.request('POST', 'http://localhost:3000/api/user/register', body)
-        .then((response) => {
-            expect(response.status).to.eq(200);
-            expect(response.body.name).to.eq('TestName');
-            expect(response.body.email).to.eq('foo@bar.com');
-            expect(response.body.password).to.eq('Test0987');
-        })
-})
+describe('/user/register', ()=>{
+    const registerEndpoint = 'http://localhost:3000/api/user/register';
 
-it('returns 400 when we hit /register with no body', ()=> {
-    cy.request({
-        method: 'POST',
-        url: 'http://localhost:3000/api/user/register',
-        failOnStatusCode: false
-    }).then((response) => {
+    it('returns 400 when with no body', ()=>{
+        cy.request({
+            method: 'POST',
+            url: registerEndpoint,
+            failOnStatusCode: false
+        }).then((response) => {
             expect(response.status).to.eq(400);
         })
+    })
+
+    it('doesnt allow user creation with bad user body', ()=> {
+        let badTestUser = {
+            name: '1',
+            email: 'foo',
+            password: '1'
+        }
+        //cypress assertions
+        cy.request({
+            method: 'POST',
+            url: registerEndpoint,
+            failOnStatusCode: false,
+            body: badTestUser
+        }).then((response) => {
+                expect(response.status).to.eq(400);
+            })
+    })
+
+    it('doesnt allow user creation with invalid email', ()=> {
+        let badTestUser = {
+            name: 'ValidName',
+            email: 'invalidEmail',
+            password: 'validPassword'
+        }
+        //cypress assertions
+        cy.request({
+            method: 'POST',
+            url: registerEndpoint,
+            failOnStatusCode: false,
+            body: badTestUser
+        }).then((response) => {
+            expect(response.status).to.eq(400);
+            expect(response.body).to.eq('"email" must be a valid email');
+        })
+    })
+
+    it('creates user with valid body', ()=> {
+        let body = {
+            name: 'TestName',
+            email: 'foo@bar.com',
+            password: 'Test0987'
+        }
+        //cypress assertions
+        cy.request('POST', registerEndpoint, body)
+            .then((response) => {
+                expect(response.status).to.eq(200);
+                expect(response.body.name).to.eq('TestName');
+                expect(response.body.email).to.eq('foo@bar.com');
+                expect(response.body.password).to.eq('Test0987');
+            })
+    })
+
 })
--- auth-api-tutorial/routes/auth.js
+++ auth-api-tutorial/routes/auth.js
@@ -1,8 +1,20 @@
 const router = require('express').Router();
 const User = require('../model/Users');
+const Joi = require('@hapi/joi');
+
+const validationSchema = Joi.object({
+    name: Joi.string().min(2).required(),
+    email: Joi.string().min(6).required().email(),
+    password: Joi.string().min(6).required()
+})
 
 //app.use('/api/register'
-router.post('/register', async (req, res) =>{
+router.post('/register', async(req, res) =>{
+    //Validate that the body content matches our requirements
+    const {error} = validationSchema.validate(req.body);
+
+    if(error) return res.status(400).send(error.details[0].message);
+
     const user = new User({
         name: req.body.name,
         email: req.body.email,
