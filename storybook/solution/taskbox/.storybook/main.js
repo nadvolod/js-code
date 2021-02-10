ea9db43ea6c47b9ad9699ce54fccf8599ef8baee
nadvolod <nadvolod@gmail.com>
2021-02-10 10:36:51 -0500

    fully working story with components

--- storybook/solution/taskbox/.storybook/main.js
+++ storybook/solution/taskbox/.storybook/main.js
@@ -1,11 +1,9 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  //👇 Location of our stories
+  stories: ['../src/components/**/*.stories.js'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/preset-create-react-app',
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/preset-create-react-app"
-  ]
-}
+};
--- storybook/solution/taskbox/.storybook/preview.js
+++ storybook/solution/taskbox/.storybook/preview.js
@@ -1,4 +1,6 @@
+import '../src/index.css'; //👈 The app's CSS file goes here
 
+//👇 Configures Storybook to log the actions( onArchiveTask and onPinTask ) in the UI.
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-}
+  actions: { argTypesRegex: '^on[A-Z].*' },
+};
