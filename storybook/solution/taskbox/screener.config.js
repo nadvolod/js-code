38938d9b19e8fce5f8c11e2cd7158f2d9e365d53
nadvolod <nadvolod@gmail.com>
2021-02-10 11:38:23 -0500

    Working Screener Components tests

---
+++ storybook/solution/taskbox/screener.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    projectRepo: 'nadvolod/js-code',
+    storybookConfigDir: '.storybook',
+    storybookStaticDir: 'public',
+    apiKey: process.env.SCREENER_API_KEY,
+    resolution: '1024x768'
+  };
