04a7d566f5e2425d79a9e87bbdf5e025ad6f6b4c
Nikolay Advolodkin <nikolayadvolodkin@sl-1506.attlocal.net>
2021-02-16 18:09:23 -0500

    2 working unit tests

---
+++ testing-js/react-components/my-app/src/__tests__/hello.test.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { render, unmountComponentAtNode } from "react-dom";
+import { act } from "react-dom/test-utils";
+
+import Hello from "../hello";
+
+let container = null;
+beforeEach(() => {
+  // setup a DOM element as a render target
+  container = document.createElement("div");
+  document.body.appendChild(container);
+});
+
+afterEach(() => {
+  // cleanup on exiting
+  unmountComponentAtNode(container);
+  container.remove();
+  container = null;
+});
+
+it("renders with or without a name", () => {
+  act(() => {
+    render(<Hello />, container);
+  });
+  expect(container.textContent).toBe("Hey, stranger");
+
+  act(() => {
+    render(<Hello name="Jenny" />, container);
+  });
+  expect(container.textContent).toBe("Hello, Jenny!");
+
+  act(() => {
+    render(<Hello name="Margaret" />, container);
+  });
+  expect(container.textContent).toBe("Hello, Margaret!");
+});
+
+// Learn more: https://reactjs.org/docs/testing-recipes.html#rendering
---
+++ testing-js/react-components/my-app/src/__tests__/react-dom.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { act } from "react-dom/test-utils";
+import {FavoriteNumber} from '../favorite-number';
+import { render, unmountComponentAtNode } from "react-dom";
+
+let container = null;
+beforeEach(() => {
+  // setup a DOM element as a render target
+  container = document.createElement("div");
+  document.body.appendChild(container);
+});
+
+//When the test ends, we want to “clean up” and unmount the tree from the document
+// https://reactjs.org/docs/testing-recipes.html#rendering
+afterEach(() => {
+  // cleanup on exiting
+  unmountComponentAtNode(container);
+  container.remove();
+  container = null;
+});
+
+// anfn tab
+test('renders a number input with a label', ()=> {
+    act(() => {
+        render(<FavoriteNumber />, container);
+      });
+    console.log(container.innerHTML);
+})
