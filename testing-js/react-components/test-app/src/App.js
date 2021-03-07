742a55f7da3938a2d510d67a54f8c30f5cd7df6a
nadvolod <nadvolod@gmail.com>
2021-03-07 14:41:47 -0500

    add a working react App to pass in different components for testing

---
+++ testing-js/react-components/test-app/src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import Hello from './components/function-component/Hello'
+
+function App() {
+  return (
+    <Hello name={'nikolay'}/>
+  );
+}
+
+export default App;
