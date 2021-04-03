e0ca5bedc907e19143d0636206133a1af84a8902
nadvolod <nadvolod@gmail.com>
2021-04-03 14:19:01 -0400

    how to test a simple button

--- testing-js/react-components/test-app/src/App.js
+++ testing-js/react-components/test-app/src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
+import {useState} from 'react'
 import Hello from './components/function-component/Hello'
 import Toggle from './components/function-component/Toggle'
 
 function App() {
+  const [buttonColor, setButtonColor] = useState('red');
+  const newButtonColor = buttonColor === 'red' ? 'blue' : 'red';
   return (
     <div>
+      <button 
+        style={{backgroundColor: buttonColor}}
+        onClick={() => setButtonColor(newButtonColor)}
+        >Change to {newButtonColor} 
+      </button>
       <Hello name={'nikolay'}/>
       <Toggle />
     </div>
--- testing-js/react-components/test-app/src/App.test.js
+++ testing-js/react-components/test-app/src/App.test.js
@@ -1,8 +1,22 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('should start in red state', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  //find an element with a role of button and text
+  const button = screen.getByRole('button', {name: 'Change to blue'})
+  //expect the background color to be red
+  expect(button).toHaveStyle({backgroundColor: 'red'})
 });
+
+test('should change color to blue on click', () => {
+  render(<App />);
+  //find an element with a role of button and text
+  const button = screen.getByRole('button', {name: 'Change to blue'})
+  //click button
+  fireEvent.click(button);
+  //expect the background color to be blue after click
+  expect(button).toHaveStyle({backgroundColor: 'blue'})
+  //
+  expect(button.textContent).toBe('Change to red')
+})
