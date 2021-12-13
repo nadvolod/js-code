9432fafb28c34cecf4b40eb29dce11e9f8b5ca78
nadvolod <nadvolod@gmail.com>
2021-12-12 19:13:46 -0500

    add ability to put O

--- tic-tac-toe/src/index.js
+++ tic-tac-toe/src/index.js
@@ -2,14 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-class Square extends React.Component {
-  render() {
-    return (
-      <button className="square" onClick={() => this.props.onClick()}>
-        {this.props.value}
-      </button>
-    );
-  }
+function Square(props) {
+  //no more render()
+  return (
+    //no more this.props and no more ()=> onClick(), instead onClick
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
 }
 
 /**
@@ -30,12 +30,13 @@ class Board extends React.Component {
     super(props);
     this.state = {
       squares: Array(9).fill(null),
+      xIsNext: true,
     };
   }
   handleClick(i) {
     const squares = this.state.squares.slice();
-    squares[i] = "X";
-    this.setState({ squares: squares });
+    squares[i] = this.state.xIsNext ? "X" : "O";
+    this.setState({ squares: squares, xIsNext: !this.state.xIsNext });
   }
   renderSquare(i) {
     return (
@@ -51,7 +52,7 @@ class Board extends React.Component {
 
   //The render method returns a description of what you want to see on the screen
   render() {
-    const status = "Next player: X";
+    const status = "Next player: " + (this.state.xIsNext ? "X" : "O");
 
     return (
       <div>
