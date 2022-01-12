f8b682f90c35c593ad576d1a92671e779ace92f1
nadvolod <nadvolod@gmail.com>
2022-01-12 12:47:29 -0500

    add routes to all pages

--- github-user-search/start/src/App.js
+++ github-user-search/start/src/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import { Dashboard, Login, PrivateRoute, AuthWrapper, Error } from './pages';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
-  return (
-    <div>
-      <Dashboard></Dashboard>
-      <Login />
-      <Error />
-    </div>
-  );
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path='/' element={<Dashboard></Dashboard>}></Route>
+                <Route path='/login' element={<Login></Login>}></Route>
+                <Route path='*' element={<Error></Error>}></Route>
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
