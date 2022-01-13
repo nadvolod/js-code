c5d60de192142e8702698c5ef8c2698bfb9e9871
nadvolod <nadvolod@gmail.com>
2022-01-12 20:00:05 -0500

    add github context and update dashboard

--- github-user-search/start/src/pages/Dashboard.js
+++ github-user-search/start/src/pages/Dashboard.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import { Info, Repos, User, Search, Navbar } from '../components';
+import { Info, User, Search, Navbar } from '../components';
 import loadingImage from '../images/preloader.gif';
 import { GithubContext } from '../context/context';
 const Dashboard = () => {
   return (
     <main>
-      <h2>Dashboard Page</h2>
+      <Navbar></Navbar>
+      <Search></Search>
+      <Info></Info>
+      <User></User>
     </main>
   );
 };
--- github-user-search/start/src/pages/Login.js
+++ github-user-search/start/src/pages/Login.js
@@ -3,22 +3,30 @@ import { useAuth0 } from '@auth0/auth0-react';
 import styled from 'styled-components';
 import loginImg from '../images/login-img.svg';
 const Login = () => {
-  return <h2>login page</h2>;
+    return (
+        <Wrapper>
+            <div className='container'>
+                <img src={loginImg} alt='github-user'></img>
+                <h1>github user</h1>
+                <button className='btn'>login</button>
+            </div>
+        </Wrapper>
+    );
 };
 const Wrapper = styled.section`
-  min-height: 100vh;
-  display: grid;
-  place-items: center;
-  .container {
-    width: 90vw;
-    max-width: 600px;
-    text-align: center;
-  }
-  img {
-    margin-bottom: 2rem;
-  }
-  h1 {
-    margin-bottom: 1.5rem;
-  }
+    min-height: 100vh;
+    display: grid;
+    place-items: center;
+    .container {
+        width: 90vw;
+        max-width: 600px;
+        text-align: center;
+    }
+    img {
+        margin-bottom: 2rem;
+    }
+    h1 {
+        margin-bottom: 1.5rem;
+    }
 `;
 export default Login;
