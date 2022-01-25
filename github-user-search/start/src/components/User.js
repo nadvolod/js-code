f0bd731d3b73b70bdff8b9b1f532a99af9423e4b
nadvolod <nadvolod@gmail.com>
2022-01-25 15:49:30 -0500

    add Card and User

--- github-user-search/start/src/components/User.js
+++ github-user-search/start/src/components/User.js
@@ -3,17 +3,24 @@ import styled from 'styled-components';
 import Card from './Card';
 import Followers from './Followers';
 const User = () => {
-  return <h2>search component</h2>;
+	return (
+		<section className='section'>
+			<Wrapper className='section-center'>
+				<Card></Card>
+				<Followers></Followers>
+			</Wrapper>
+		</section>
+	);
 };
 
 const Wrapper = styled.div`
-  padding-top: 2rem;
-  display: grid;
-  gap: 3rem 2rem;
-  @media (min-width: 992px) {
-    grid-template-columns: 1fr 1fr;
-  }
-  /* align-items: start; */
+	padding-top: 2rem;
+	display: grid;
+	gap: 3rem 2rem;
+	@media (min-width: 992px) {
+		grid-template-columns: 1fr 1fr;
+	}
+	/* align-items: start; */
 `;
 
 export default User;
