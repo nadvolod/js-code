f2ca6a8f3f2632dafd7c71867fb1fc86c1ab33e4
nadvolod <nadvolod@gmail.com>
2022-01-26 14:14:00 -0500

    all the finished code

--- birthday-reminder/src/App.js
+++ birthday-reminder/src/App.js
@@ -2,7 +2,19 @@ import React, { useState } from 'react';
 import data from './data';
 import List from './List';
 function App() {
-  return <h2>reminder project setup</h2>;
+	const [people, setPeople] = useState(data);
+
+	return (
+		<main>
+			<section className='container'>
+				<h1>{people.length} birthdays</h1>
+				<List people={people}></List>
+				<button data-testid='clear' onClick={() => setPeople([])}>
+					clear all
+				</button>
+			</section>
+		</main>
+	);
 }
 
 export default App;
