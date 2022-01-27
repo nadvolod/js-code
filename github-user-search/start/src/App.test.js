f572061f8e6a903babeab19e2527f914b7a32942
nadvolod <nadvolod@gmail.com>
2022-01-26 19:55:55 -0500

    add test for back-home button

--- github-user-search/start/src/App.test.js
+++ github-user-search/start/src/App.test.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import Error from './pages/Error';
 
-test('404 page back-home button goes to correct url', () => {
-    const { getByTestId } = render(<Error />);
-    const button = getByTestId('back-home');
-    expect(button.href).toContain('http://localhost');
+test('404 page back-home button back home', () => {
+	const { getByTestId } = render(<Error />);
+	const button = getByTestId('back-home');
+	expect(button.href).toContain('http://localhost');
 });
