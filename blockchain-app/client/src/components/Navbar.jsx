90bcf43db13f3d6d25c7c54b279a4f604c0934eb
nadvolod <nadvolod@gmail.com>
2022-02-12 13:45:30 -0500

    Add styling to index.css and add icons

--- blockchain-app/client/src/components/Navbar.jsx
+++ blockchain-app/client/src/components/Navbar.jsx
@@ -1,5 +1,8 @@
 // rafce
 import React from 'react';
+import { HiMenuAlt4 } from 'react-icons/hi';
+import { AiOutlineClose } from 'react-icons/ai';
+import logo from '../../images/logo.png';
 
 const Navbar = () => {
 	return <div>Navbar</div>;
