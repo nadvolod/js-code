1676ac3ea3c534e8d0d0f7e3f5d5b63398e60635
nadvolod <nadvolod@gmail.com>
2022-02-11 20:06:38 -0500

    change app.js to arrow function

--- blockchain-app/client/src/App.jsx
+++ blockchain-app/client/src/App.jsx
@@ -1,15 +1,12 @@
 import { useState } from 'react';
-import logo from './logo.svg';
 import './index.css';
 
-function App() {
-	const [count, setCount] = useState(0);
-
+const App = () => {
 	return (
 		<div className='App'>
 			<h1 class='text-3xl font-bold underline'>Hello world!</h1>
 		</div>
 	);
-}
+};
 
 export default App;
