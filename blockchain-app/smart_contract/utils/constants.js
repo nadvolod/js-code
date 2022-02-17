beacd5b6fd03711b9f138514e981b4f721db78dc
nadvolod <nadvolod@gmail.com>
2022-02-17 14:05:22 -0500

    Add constants and Transactions for contracts

--- blockchain-app/smart_contract/utils/constants.js
+++ blockchain-app/smart_contract/utils/constants.js
@@ -1,2 +1,5 @@
+import abi from './Transactions.json';
+
+export const contractABI = abi.abi;
 // export const contractAddress = '0xB1eA0Bc0Bbd554d921Ac6778775208A39259831A';
 export const contractAddress = '0x904F6eD53Bb20379136481caAb9393aE131a049e';
