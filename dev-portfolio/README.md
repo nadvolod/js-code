c07f07ecf1f540ff1701c3d9f74d731859cb65f3
nadvolod <nadvolod@gmail.com>
2022-02-23 16:10:11 -0500

    Update accomplishments

--- dev-portfolio/README.md
+++ dev-portfolio/README.md
@@ -15,3 +15,5 @@ npm i && npm run dev
 - Styled components (each component has a corresponding ComponentStyles.js file)
 
 [Thanks to JavaScript Mastery for the inspiration](https://www.youtube.com/watch?v=OPaLnMw2i_0&list=PL1YmAbfxmHuOsV3zmAnncnql3MMsIeO2_&index=24)
+
+## Testing
--- dev-portfolio/src/components/Acomplishments/Acomplishments.js
+++ dev-portfolio/src/components/Acomplishments/Acomplishments.js
@@ -1,31 +1,31 @@
-import React from 'react';
+import React from "react";
 
 import {
-	Section,
-	SectionDivider,
-	SectionTitle,
-} from '../../styles/GlobalComponents';
-import { Box, Boxes, BoxNum, BoxText } from './AcomplishmentsStyles';
+  Section,
+  SectionDivider,
+  SectionTitle,
+} from "../../styles/GlobalComponents";
+import { Box, Boxes, BoxNum, BoxText } from "./AcomplishmentsStyles";
 
 const data = [
-	{ number: '100,000', text: 'SDETs Trained' },
-	{ number: 50, text: 'Open Source Projects' },
-	{ number: 5, text: 'Years Being Awared Top Automation Engineer' },
-	{ number: 5000, text: 'Github Stars' },
+  { number: "100,000", text: "SDETs Trained" },
+  { number: 50, text: "Open Source Projects" },
+  { number: 5, text: "Years Being Awared Top Automation Engineer" },
+  { number: 250, text: "Github Stars" },
 ];
 
 const Acomplishments = () => (
-	<Section>
-		<SectionTitle>Personal Accomplishments</SectionTitle>
-		<Boxes>
-			{data.map((card, index) => (
-				<Box key={index}>
-					<BoxNum>{card.number}+</BoxNum>
-					<BoxText>{card.text}</BoxText>
-				</Box>
-			))}
-		</Boxes>
-	</Section>
+  <Section>
+    <SectionTitle>Personal Accomplishments</SectionTitle>
+    <Boxes>
+      {data.map((card, index) => (
+        <Box key={index}>
+          <BoxNum>{card.number}+</BoxNum>
+          <BoxText>{card.text}</BoxText>
+        </Box>
+      ))}
+    </Boxes>
+  </Section>
 );
 
 export default Acomplishments;
