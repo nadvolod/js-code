f0dc31a70e516f758551e45a977ccccbdbce6cae
nadvolod <nadvolod@gmail.com>
2022-04-16 15:44:31 -0400

    update images to be smaller

--- dev-portfolio/src/constants/projects.js
+++ dev-portfolio/src/constants/projects.js
@@ -1,56 +1,54 @@
 const projects = [
-  {
-    title: "Ultimate QA",
-    description:
-      "Created a community of 100K+ developers and SDETS to help each other create better code.",
-    image: "/images/ultimateqa.png",
-    tags: ["Wordpress", "Testing", "Development"],
-    source: "#",
-    visit: "https://ultimateqa.com/",
-    id: 0,
-  },
-  {
-    title: "Github User Search",
-    description:
-      "Created a web application that allows users to search for Github users and see their profile information. Tested and deployed.",
-    image: "/images/github-users-search.png",
-    tags: ["React", "REST API", "Netlify", "CypressIO"],
-    source:
-      "https://github.com/nadvolod/js-code/tree/master/github-user-search",
-    visit:
-      "https://61d9fb1e9d115dbf5b790ad4--distracted-wilson-20a730.netlify.app/",
-    id: 1,
-  },
-  {
-    title: "Sauce Bindings",
-    description:
-      "Co-created an open source project that allows users to interact with Sauce Labs through a beautiful API.",
-    image: "/images/sauce-bindings.png",
-    tags: ["Java", "C#", "Github Actions"],
-    source: "https://github.com/saucelabs/sauce_bindings",
-    visit: "https://opensource.saucelabs.com/sauce_bindings/docs/overview.html",
-    id: 2,
-  },
-  {
-    title: "Birthday Reminder App",
-    description:
-      "A simple app that allows users to track their friends' birthdays.",
-    image: "/images/birthday-reminder.jpeg",
-    tags: ["React", "Node", "Netlify", "WebdriverIO"],
-    source: "https://github.com/nadvolod/js-code/tree/master/birthday-reminder",
-    visit: "https://laughing-feynman-11feb4.netlify.app/",
-    id: 3,
-  },
-  {
-    title: "Dev Portfolio Website",
-    description:
-      "A mobile-first, responsive, sleek website to show off my skills.",
-    image: "/images/portfolio.png",
-    tags: ["NextJS", "Node", "Cypress", "Happo.io"],
-    source: "https://github.com/nadvolod/js-code/tree/master/dev-portfolio",
-    visit: "https://nikolay.tech",
-    id: 4,
-  },
-];
+    {
+        title: 'Ultimate QA',
+        description:
+            'Created a community of 100K+ developers and SDETS to help each other create better code.',
+        image: '/images/ultimateqa.webp',
+        tags: ['Wordpress', 'Testing', 'Development'],
+        source: '#',
+        visit: 'https://ultimateqa.com/',
+        id: 0,
+    },
+    {
+        title: 'Github User Search',
+        description:
+            'Created a web application that allows users to search for Github users and see their profile information. Tested and deployed.',
+        image: '/images/github-users-search.webp',
+        tags: ['React', 'REST API', 'Netlify', 'CypressIO'],
+        source: 'https://github.com/nadvolod/js-code/tree/master/github-user-search',
+        visit: 'https://61d9fb1e9d115dbf5b790ad4--distracted-wilson-20a730.netlify.app/',
+        id: 1,
+    },
+    {
+        title: 'Sauce Bindings',
+        description:
+            'Co-created an open source project that allows users to interact with Sauce Labs through a beautiful API.',
+        image: '/images/sauce-bindings.webp',
+        tags: ['Java', 'C#', 'Github Actions'],
+        source: 'https://github.com/saucelabs/sauce_bindings',
+        visit: 'https://opensource.saucelabs.com/sauce_bindings/docs/overview.html',
+        id: 2,
+    },
+    {
+        title: 'Birthday Reminder App',
+        description:
+            "A simple app that allows users to track their friends' birthdays.",
+        image: '/images/birthday-reminder.webp',
+        tags: ['React', 'Node', 'Netlify', 'WebdriverIO'],
+        source: 'https://github.com/nadvolod/js-code/tree/master/birthday-reminder',
+        visit: 'https://laughing-feynman-11feb4.netlify.app/',
+        id: 3,
+    },
+    {
+        title: 'Dev Portfolio Website',
+        description:
+            'A mobile-first, responsive, sleek website to show off my skills.',
+        image: '/images/portfolio.webp',
+        tags: ['NextJS', 'Node', 'Cypress', 'Happo.io'],
+        source: 'https://github.com/nadvolod/js-code/tree/master/dev-portfolio',
+        visit: 'https://nikolay.tech',
+        id: 4,
+    },
+]
 // this is the way to work with Jest
-module.exports = { projects };
+module.exports = { projects }
