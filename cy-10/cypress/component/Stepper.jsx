ab0af33a286704e0b33006d91bb73f7e5750e793
nadvolod <nadvolod@gmail.com>
2022-06-08 10:55:05 -0400

    working component tests

--- cy-10/cypress/component/Stepper.jsx
+++ cy-10/cypress/component/Stepper.jsx
@@ -1,15 +1,27 @@
 import { useState } from "react";
 
-export default function Stepper({ initial = 0 }) {
+export default function Stepper({ initial = 0, onChange = () => {} }) {
   const [count, setCount] = useState(initial);
+
+  const increment = () => {
+    const newCount = count + 1;
+    setCount(newCount);
+    onChange(newCount);
+  };
+
+  const decrement = () => {
+    const newCount = count - 1;
+    setCount(newCount);
+    onChange(newCount);
+  };
 
   return (
     <div data-testid="stepper">
-      <button aria-label="decrement" onClick={() => setCount(count - 1)}>
+      <button aria-label="decrement" onClick={decrement}>
         -
       </button>
       {count}
-      <button aria-label="increment" onClick={() => setCount(count + 1)}>
+      <button aria-label="increment" onClick={increment}>
         +
       </button>
     </div>
