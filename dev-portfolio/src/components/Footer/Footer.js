0686e3cb38715da21640cea81ae85a09f7f2b797
nadvolod <nadvolod@gmail.com>
2023-02-05 08:47:18 -0500

    change mailto link

--- dev-portfolio/src/components/Footer/Footer.js
+++ dev-portfolio/src/components/Footer/Footer.js
@@ -1,36 +1,39 @@
-import React from "react";
-import SocialDetails from "../Header/SocialDetails";
+import React from 'react'
+import SocialDetails from '../Header/SocialDetails'
 
 import {
-  CompanyContainer,
-  FooterWrapper,
-  LinkColumn,
-  LinkItem,
-  LinkList,
-  LinkTitle,
-  Slogan,
-  SocialIconsContainer,
-} from "./FooterStyles";
+    CompanyContainer,
+    FooterWrapper,
+    LinkColumn,
+    LinkItem,
+    LinkList,
+    LinkTitle,
+    Slogan,
+    SocialIconsContainer,
+} from './FooterStyles'
 
 const Footer = () => {
-  return (
-    <FooterWrapper>
-      <LinkList>
-        <LinkColumn>
-          <LinkTitle>Email</LinkTitle>
-          <LinkItem href="mailto:nadvolod@gmail.com">
-            click to email me
-          </LinkItem>
-        </LinkColumn>
-      </LinkList>
-      <SocialIconsContainer>
-        <CompanyContainer>
-          <Slogan>Innovation</Slogan>
-        </CompanyContainer>
-        <SocialDetails></SocialDetails>
-      </SocialIconsContainer>
-    </FooterWrapper>
-  );
-};
+    return (
+        <FooterWrapper>
+            <LinkList>
+                <LinkColumn>
+                    <LinkTitle>Email</LinkTitle>
+                    <LinkItem
+                        data-testid="mailto"
+                        href="mailto:nikolay@ultimateqa.com"
+                    >
+                        click to work with me
+                    </LinkItem>
+                </LinkColumn>
+            </LinkList>
+            <SocialIconsContainer>
+                <CompanyContainer>
+                    <Slogan>Innovation</Slogan>
+                </CompanyContainer>
+                <SocialDetails></SocialDetails>
+            </SocialIconsContainer>
+        </FooterWrapper>
+    )
+}
 
-export default Footer;
+export default Footer
